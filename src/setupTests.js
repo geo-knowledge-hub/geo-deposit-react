ae9fe2c81be4e6fd69171777d3de251a896c6626
Felipe Menino <efelipecarlos@gmail.com>
2022-04-23 14:58:03 -0300

    tests: setup tests

---
+++ src/setupTests.js
@@ -0,0 +1,15 @@
+/*
+ * This file is part of GEO-Deposit-React.
+ * Copyright (C) 2022 GEO Secretariat.
+ *
+ * GEO-Deposit-React is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ *
+ * Code adapted from: https://github.com/inveniosoftware/react-invenio-deposit/blob/84956cbf2e2bc28cf6a9834ecc5df37c824bd6ff/src/setupTests.js
+ */
+
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
